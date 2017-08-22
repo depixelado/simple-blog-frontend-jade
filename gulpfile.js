9391924af8b79c75286e83098142d7bf94c489c9
Daniel Jimenez <d.jimenezcabrera@reply.com>
2017-08-22 13:51:25 +0100

    Add babel gulp task to transpile javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
+const babel = require('gulp-babel');
+const clean = require('gulp-clean');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
-const clean = require('gulp-clean');
 
 /**
  * @author Daniel Jimenez <jimenezdaniel87@gmail.com>
  * @function clean
- * @description Clean dist folder 
+ * @description Delete dist folder 
  */
 gulp.task('clean', () =>
   gulp.src(
     // Sources to empty/remove
     [
-      'dist/**/*',
       './dist',
     ],
     {
       read: false,
+      force: true,
     }
   )
     .pipe(clean())
@@ -55,11 +56,47 @@ gulp.task('sass:watch', () =>
 
 /**
  * @author Daniel Jimenez <jimenezdaniel87@gmail.com>
+ * @function babel
+ * @description Transpile javascript code throught babel
+ */
+gulp.task('babel', () =>
+  gulp.src(
+    [
+      './**/*.js',
+      '!gulpfile.js',
+    ]
+  )
+    .pipe(babel({
+      presets: ['es2015'],
+    }))
+    .pipe(gulp.dest('dist'))
+);
+
+/**
+ * @author Daniel Jimenez <jimenezdaniel87@gmail.com>
+ * @function babel:watch
+ * @description Executes babel task when js files are changed
+ */
+gulp.task('babel:watch', () =>
+  gulp.watch(
+    [
+      './**/*.js',
+      '!gulpfile.js',
+    ],
+    [
+      'babel'
+    ]
+  )
+);
+
+/**
+ * @author Daniel Jimenez <jimenezdaniel87@gmail.com>
  * @function default
  * @description Execute a list of tasks by default
  */
 gulp.task('default', [
-  'clean',
   'sass',
   'sass:watch',
+  'babel',
+  'babel:watch'
 ]);
