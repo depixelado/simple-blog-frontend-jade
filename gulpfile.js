ab045b89053b6254bc64477f77045d9237bcdcd8
Daniel Jimenez <d.jimenezcabrera@reply.com>
2017-08-22 12:01:29 +0100

    Add clean gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,47 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const clean = require('gulp-clean');
 
+/**
+ * @author Daniel Jimenez <jimenezdaniel87@gmail.com>
+ * @function clean
+ * @description Clean dist folder 
+ */
+gulp.task('clean', () =>
+  gulp.src(
+    // Sources to empty/remove
+    [
+      'dist/**/*',
+      './dist',
+    ],
+    {
+      read: false,
+    }
+  )
+    .pipe(clean())
+);
+
+/**
+ * @author Daniel Jimenez <jimenezdaniel87@gmail.com>
+ * @function sass
+ * @description Process SCSS files into CSS
+ */
 gulp.task('sass', () =>
-  gulp.src(['assets/scss/**/*.scss', '!assets/scss/**/_*.scss'])
+  gulp.src([
+    // Files to include
+    'assets/scss/**/*.scss',
+    // Files to omit
+    '!assets/scss/**/_*.scss',
+  ])
+    // Process Sass files
     .pipe(sass())
+    // Generated files destination source
     .pipe(gulp.dest('dist/public/css'))
 );
 
-gulp.task('default', ['sass']);
+/**
+ * @author Daniel Jimenez <jimenezdaniel87@gmail.com>
+ * @function default
+ * @description Execute a list of tasks by default
+ */
+gulp.task('default', ['clean', 'sass']);
